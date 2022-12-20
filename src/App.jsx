91135a2c4cf80170ec0c4c6085ae9091fc4b4b12
Andrey Uvarov <andrey-1@ukr.net>
2022-12-20 22:38:30 +0200

    comit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Statistics from './components/Statistics';
 import FeedbackOptions from './components/FeedbackOptions';
 import Section from './components/Section';
 import Notification from './components/Notification';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const { good, neutral, bad } = state;
+  const onLeaveFeedback = option => {
+    setState(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
   };
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return (this.state.good / this.countTotalFeedback()) * 100 || 0;
+  const countPositiveFeedbackPercentage = () => {
+    return (good / countTotalFeedback()) * 100 || 0;
   };
 
-  render() {
-    const total = this.countTotalFeedback();
-    const positive = this.countPositiveFeedbackPercentage();
-    const { good, neutral, bad } = this.state;
-    const arrayKey = Object.keys(this.state);
-    return (
-      <div className="Feedback">
-        <Section title="Please leave feedback">
-          {/* БЛОК КНОПОК */}
-          <FeedbackOptions
-            options={arrayKey}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const total = countTotalFeedback();
+  const positive = countPositiveFeedbackPercentage();
+  const arrayKey = Object.keys(state);
+  return (
+    <div className="Feedback">
+      <Section title="Please leave feedback">
+        {/* БЛОК КНОПОК */}
+        <FeedbackOptions options={arrayKey} onLeaveFeedback={onLeaveFeedback} />
+        {/* БЛОК КНОПОК */}
+        {!total ? (
+          // Предупреждение
+          <div>
+            <Notification message="There is no feedback"></Notification>
+          </div>
+        ) : (
+          // Предупреждение
+          // {/* СТАТИСТИКА */}
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positive}
           />
-          {/* БЛОК КНОПОК */}
-          {!total ? (
-            // Предупреждение
-            <div>
-              <Notification message="There is no feedback"></Notification>
-            </div>
-          ) : (
-            // Предупреждение
-            // {/* СТАТИСТИКА */}
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positive}
-            />
-          )}
-          {/* СТАТИСТИКА */}
-        </Section>
-      </div>
-    );
-  }
-}
+        )}
+        {/* СТАТИСТИКА */}
+      </Section>
+    </div>
+  );
+};
+
 export default App;
